5badd54dbb56a868e47dff7f2c56412ae60865ea
jboes440 <jboesiger@icam.ch>
2026-01-30 15:15:07 +0000

    ML

---
+++ 03-Machine Learning/00_Init.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC USE CATALOG jul_lakehouse;
+# MAGIC
+# MAGIC CREATE DATABASE IF NOT EXISTS jul_lakehouse.ml_sandbox;
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Upload now the data files in the ML Sandbox
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+
+file_path = f"/Volumes/jul_lakehouse/ml_sandbox/Data/train.csv"
+train_df = spark.read.csv(file_path, header="true", inferSchema="true")
+train_df = train_df.withColumn("PassengerId", col("PassengerId").cast("string")) \
+                   .withColumn("VIP", col("VIP").cast("int")) \
+                   .withColumn("CryoSleep", col("CryoSleep").cast("int")) \
+                   .withColumn("Transported", col("Transported").cast("int")) 
+                   
